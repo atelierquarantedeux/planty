bf6967d4e6278b199d361909dede18f0c204c97a
atelierquarantedeux <atelierquarantedeux@gmail.com>
2023-07-10 19:28:06 +0200

    finitions image formulaire

--- wp-content/themes/index.php
+++ wp-content/themes/index.php
@@ -2,18 +2,3 @@
 // Silence is golden.
 
 
-add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
-
-function add_admin_link( $items, $args ) {
-
-    if (is_user_logged_in() && $args->theme_location == 'menu-1') {
-
-        $items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
-
-    }
-
-    return $items;
-
-}
-
-?>
